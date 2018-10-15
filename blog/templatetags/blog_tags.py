13633bbff6e48708a0430340a864f288db1f69f0
lixiang30 <534551900@qq.com>
2018-10-15 23:19:31 +0800

    模板标签最新博客和推荐博客函数

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,13 @@
+from django import template
+from ..models import Entry,Category,Tag
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_recent_entries(num=5):
+    return Entry.objects.all().order_by('-created_time')[:num]
+
+@register.simple_tag
+def get_popular_entries(num=5):
+    return Entry.objects.all().order_by('-visiting')[:num]
