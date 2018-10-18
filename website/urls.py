92897d931b4812ed97fa9ca4cc40341e08238926
lixiang30 <534551900@qq.com>
2018-10-18 21:21:36 +0800

    403,404,500页面url以及站点地图url

--- website/urls.py
+++ website/urls.py
@@ -13,9 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from blog import views as blog_views
+
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
+from blog.models import Entry
+
+
+info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field': 'modified_time'
+}
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^blog/',include('blog.urls')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.6)}},
+      name='django.contrib.sitemaps.views.sitemap'),       #站点地图
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+handler403 = blog_views.permission_denied
+handler404 = blog_views.page_not_found
+handler500 = blog_views.page_error
+
+
+
+
